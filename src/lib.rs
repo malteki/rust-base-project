f10a2ae51ca0a0190889d643bc093b0a353756bc
malteki <mki.dev@web.de>
2024-11-08 14:17:27 +0100

    added env_vars

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,15 @@
+//  moudles
+
 pub mod errors;
+
+// constants
+
+pub mod env_vars {
+    pub const CARGO_PKG_NAME: &'static str = env!("CARGO_PKG_NAME");
+
+    pub const CARGO_PKG_VERSION: &'static str = env!("CARGO_PKG_VERSION");
+
+    pub const CARGO_PKG_DESCRIPTION: &'static str = env!("CARGO_PKG_DESCRIPTION");
+
+    pub const CARGO_PKG_AUTHORS: &'static str = env!("CARGO_PKG_AUTHORS");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,18 @@
 use tracing::info;
 
+use name::env_vars::*;
+
+//
+
 fn init_env() {
     std::env::set_var("RUST_LOG", "trace");
 }
 
-#[tokio::main]
+#[tokio::main(flavor = "multi_thread", worker_threads = 4)]
 async fn main() -> anyhow::Result<()> {
     init_env();
     tracing_subscriber::fmt::init();
-
-    info!("Hello, World!");
+    info!("running {CARGO_PKG_NAME} v{CARGO_PKG_VERSION}");
 
     Ok(())
 }
